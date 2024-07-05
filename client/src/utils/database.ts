97c7be58f6b6c85838caaa1d0241cdc55e978873
igachev <ivophoenix1135@gmail.com>
2024-07-06 00:56:40 +0300

    updated database.ts
    added register and login functions

--- client/src/utils/database.ts
+++ client/src/utils/database.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql2'
+import mysql, { ResultSetHeader } from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -12,5 +12,37 @@ const pool = mysql.createPool({
  export async function getAllUsers() {
     const [rows] = await pool.query("select * from users")
     return rows
+  }
+
+  export async function getUser(userId: number) {
+    const result = await pool.query<ResultSetHeader>("SELECT * FROM `users` WHERE id=?",[userId])
+    const resultData: any = result[0]
+    const userData:{id:number;email:string} = {id: resultData[0].id, email: resultData[0].email}
+    return userData
+  }
+
+  export async function isRegistered(email:string) {
+    const result = await pool.query<ResultSetHeader>("SELECT * FROM `users` WHERE email=?",[email])
+    const userData: any = result[0]
+    if(userData[0] != undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  export async function registerUser(email:string, password: string) {
+    const result = await pool.query<ResultSetHeader>("INSERT INTO `users` (email,password) VALUES(?,?)",[email,password])
+    const userId = result[0].insertId
+    const userData:any = await getUser(userId)
+    return userData
+  }
+
+  export async function loginUser(email: string, password: string) {
+    const result = await pool.query<ResultSetHeader>("SELECT * FROM `users` WHERE email=? AND password=?",[email,password])
+    const userData: any = result[0]
+    if(userData[0] == undefined) {
+      throw new Error("Invalid email or password!")
+    }
+    return userData[0]
   }
   
