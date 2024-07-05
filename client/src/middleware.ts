51c2f64ee429885fc1e61956ce80a9f84b24fea3
igachev <ivophoenix1135@gmail.com>
2024-07-06 00:55:36 +0300

    updated middleware
    added /api/register , /api/login as public routes

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,11 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', "/","/api/users"]);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', "/","/api/users","/api/register","/api/login"]);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
     auth().protect();
   }
+
 });
 
 export const config = {
