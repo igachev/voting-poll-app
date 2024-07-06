4cae6bfd81bfd977e9aea2caaa2494adca45a337
igachev <ivophoenix1135@gmail.com>
2024-07-07 00:19:38 +0300

    I created route /polls

---
+++ client/src/app/polls/page.tsx
@@ -0,0 +1,13 @@
+import Polls from '@/components/Polls/Polls'
+import React from 'react'
+
+const PollsPage = () => {
+  return (
+    <div>
+        <h1>List Of Polls</h1>
+        <Polls />
+    </div>
+  )
+}
+
+export default PollsPage
