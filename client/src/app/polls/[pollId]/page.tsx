49aa6095f967c859d87f004f1494b5a8a183c6c9
igachev <ivophoenix1135@gmail.com>
2024-07-11 12:09:22 +0300

    updated page /polls/:pollId
    1) display poll details
    2) added button Delete Poll
    3) if poll is successfully deleted redirect user to page /polls
    4) if error occurs when deleting the poll display that error

--- client/src/app/polls/[pollId]/page.tsx
+++ client/src/app/polls/[pollId]/page.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 interface PollOption {
-    pollOption: string;
+    poll_option: string;
 }
 
 interface Poll {
@@ -22,6 +22,32 @@ const PollDetails = ({
 
     const [poll,setPoll] = useState<Poll | null>(null)
     const router = useRouter()
+    const [error, setError] = useState<string | null>(null)
+
+    async function deletePoll() {
+      const userData = localStorage?.getItem("userData")
+
+      let userId = 0;
+      if(userData) {
+        const data = JSON.parse(userData)
+        userId = parseInt(data.id)
+      }
+      
+      try {
+        const response = await fetch(`/api/polls/${params.pollId}/${userId}`, {
+          method: 'DELETE'
+        })
+
+        if (!response.ok) {
+          const errorData = await response.json()
+          throw new Error(errorData.message || 'Failed to delete poll')
+        }
+
+        router.push("/polls")
+      } catch (err: any) {
+        setError(err.message)
+      }
+    }
 
     useEffect(() => {
         fetch(`/api/polls/${params.pollId}`)
@@ -35,7 +61,21 @@ const PollDetails = ({
   return (
     <div>
         <h1>Poll Details</h1>
-        
+        <section>
+          <h3>Title: {poll?.poll_title}</h3>
+          <h5>Description: {poll?.poll_description}</h5>
+          <div>{poll?.pollOptions.length! > 0 && poll?.pollOptions.map((pollOption) => (
+            <div key={pollOption.poll_option}>
+              <button>{pollOption.poll_option}</button>
+            </div>
+          ))}</div>
+        </section>
+        <button onClick={deletePoll}>Delete Poll</button>
+        {error && (
+          <div>
+            <p>{error}</p>
+          </div>
+        )}
     </div>
   )
 }
