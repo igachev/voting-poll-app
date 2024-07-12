c3f3406838288939a230f7ac961845caa82244a5
igachev <ivophoenix1135@gmail.com>
2024-07-12 21:53:45 +0300

    edited endpoint /api/polls/:pollId/:userId
    added POST endpoint used for voting

--- client/src/app/api/polls/[pollId]/[userId]/route.ts
+++ client/src/app/api/polls/[pollId]/[userId]/route.ts
@@ -1,4 +1,4 @@
-import { deletePoll } from "@/utils/database"
+import { deletePoll, pollVote } from "@/utils/database"
 import { NextRequest, NextResponse } from "next/server"
 
 
@@ -8,6 +8,18 @@ export const DELETE = async(req: NextRequest, { params }: { params: {pollId: str
         const userId = params.userId
         const deletedPoll = await deletePoll(parseInt(pollId),parseInt(userId))
         return NextResponse.json(deletedPoll, {status: 200})
+    } catch (err: any) {
+        return NextResponse.json({message: err.message},{status: 500})
+    }
+}
+
+export const POST = async(req: NextRequest, { params }: { params: {pollId: string, userId: string} }): Promise<NextResponse> => {
+    try {
+        const pollId = params.pollId
+        const userId = params.userId
+        const {selectedOption} = await req.json()
+        const voteForPoll = await pollVote(parseInt(userId),parseInt(pollId),selectedOption)
+        return NextResponse.json(voteForPoll,{status: 201})
     } catch (err: any) {
         return NextResponse.json({message: err.message},{status: 500})
     }
