3f7621d3cf86bd2958ac6ff963066f8d79cc77d9
igachev <ivophoenix1135@gmail.com>
2024-07-13 17:51:32 +0300

    added GET endpoint /api/polls/:pollId/:userId
    returns all votes for specific poll.

--- client/src/app/api/polls/[pollId]/[userId]/route.ts
+++ client/src/app/api/polls/[pollId]/[userId]/route.ts
@@ -1,4 +1,4 @@
-import { deletePoll, pollVote } from "@/utils/database"
+import { deletePoll, getVotes, pollVote } from "@/utils/database"
 import { NextRequest, NextResponse } from "next/server"
 
 
@@ -20,6 +20,16 @@ export const POST = async(req: NextRequest, { params }: { params: {pollId: strin
         const {selectedOption} = await req.json()
         const voteForPoll = await pollVote(parseInt(userId),parseInt(pollId),selectedOption)
         return NextResponse.json(voteForPoll,{status: 201})
+    } catch (err: any) {
+        return NextResponse.json({message: err.message},{status: 500})
+    }
+}
+
+export const GET = async (req: NextRequest, { params }: { params: {pollId: string} }): Promise<NextResponse> => {
+    try {
+        const pollId = params.pollId
+        const votes = await getVotes(parseInt(pollId))
+        return NextResponse.json(votes,{status: 200})
     } catch (err: any) {
         return NextResponse.json({message: err.message},{status: 500})
     }
