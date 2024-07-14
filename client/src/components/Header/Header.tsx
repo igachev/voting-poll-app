acd405e5d4b4ceb88268caa1fe7affa6f01c85e2
igachev <ivophoenix1135@gmail.com>
2024-07-14 23:43:49 +0300

    edited Header.tsx component
    1) used shadcn navigation menu
    2) added tailwind css classes to some elements
    3) added Roboto_Mono font and used it only in Header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,43 +2,103 @@
 import { SignedIn, useAuth, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { Roboto_Mono } from 'next/font/google'
+
+export const roboto = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+  style:['normal'],
+  variable: '--font-roboto-mono'
+})
+
 
 const Header = () => {
-    let {isSignedIn} = useAuth()
+  let { isSignedIn } = useAuth()
 
-    useEffect(() => {
-      if(!isSignedIn) {
-        localStorage.removeItem("userData")
-      }
-    },[isSignedIn])
+  useEffect(() => {
+    if (!isSignedIn) {
+      localStorage.removeItem("userData")
+    }
+  }, [isSignedIn])
 
   return (
-    <header>
-          <nav>
-            <ul>
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/polls">Polls</Link></li>
-              {isSignedIn && (
-                <>
-                <li>
-                <SignedIn>
-                <UserButton afterSignOutUrl="/" />
-                </SignedIn>
-              </li>
-              <li><Link href="/create-poll">Create Poll</Link></li>
-                </>
-              )}
+    <header className={`${roboto.variable} font-roboto-mono w-full bg-slate-600 pt-2 pb-2`}>
+      <NavigationMenu >
+        <NavigationMenuList className='w-full flex flex-col justify-center sm:flex-row'>
 
-            {!isSignedIn && (
-                <>
-                  <li><Link href="/sign-in">Sign In</Link></li>
-                  <li><Link href="/sign-up">Sign Up</Link></li>
-                </>
-            )}
-            
-            </ul>
-          </nav>
-    </header>    
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref >
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link href="/polls" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Polls
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          {isSignedIn && (
+            <>
+
+          <NavigationMenuItem>
+            <Link href="/create-poll" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Create Poll
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem className='mt-1'>
+            <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+          </NavigationMenuItem>
+
+            </>
+          )}
+
+          {!isSignedIn && (
+            <>
+            <NavigationMenuItem>
+            <Link href="/sign-in" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Login
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link href="/sign-up" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Register
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+            </>
+          )}
+
+        </NavigationMenuList>
+      </NavigationMenu>
+
+    </header>
   )
 }
 
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        roboto: ['var(--font-roboto-mono)']
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
