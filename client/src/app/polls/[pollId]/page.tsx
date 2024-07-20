847d8f1078cc784d51944a05c6cf562780fbc0f5
igachev <ivophoenix1135@gmail.com>
2024-07-20 21:35:06 +0300

    edited route page /polls/:pollId
    I used a PieChart from recharts to show what options users voted for

--- client/src/app/polls/[pollId]/page.tsx
+++ client/src/app/polls/[pollId]/page.tsx
@@ -1,7 +1,33 @@
 "use client"
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,PureComponent } from 'react'
+import { ResponsiveContainer, PieChart, Pie, Legend, Tooltip, Cell } from 'recharts';
+
+interface CustomizedLabelProps {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  percent: number;
+  index: number;
+}
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }: CustomizedLabelProps) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 interface PollOption {
     poll_option: string;
@@ -19,6 +45,11 @@ interface Vote {
     [key: string]: number;
 }
 
+interface FormatVote {
+  name: string;
+  value: number;
+}
+
 const PollDetails = ({
     params,
   }: {
@@ -29,7 +60,7 @@ const PollDetails = ({
     const router = useRouter()
     const [error, setError] = useState<string | null>(null)
     const [selectedOption,setSelectedOption] = useState<string>()
-    const [votes,setVotes] = useState<Vote[]>([])
+    const [votes,setVotes] = useState<FormatVote[]>([])
     const [isClicked,setIsClicked] = useState<boolean>(false)
 
    function onSelectedOption(selectedOption: string) {
@@ -82,6 +113,9 @@ const PollDetails = ({
           throw new Error(errorData.message)
         }
 
+        const newVote: FormatVote = {name: selectedOption!,value: 1}
+        setVotes((oldVotes) => [...oldVotes,newVote])
+
       } catch (err: any) {
         setError(err.message)
       }
@@ -112,12 +146,12 @@ const PollDetails = ({
 
   return (
     <div className='min-h-[800px] bg-stone-700 text-white'>
-    <h1 className='text-4xl text-center p-2'>Poll Details</h1>
-    <div className='bg-stone-500 min-h-[500px] w-5/6 flex flex-col justify-center items-center mx-auto rounded-md'>
+    <h1 className='text-2xl sm:text-4xl text-center p-2'>Poll Details</h1>
+    <div className='bg-stone-500 min-h-[500px] w-5/6 flex flex-col justify-center items-center mx-auto rounded-md shadow-inner shadow-stone-400'>
         
-        <section className='w-5/6 bg-slate-500 p-4 flex flex-col justify-center items-center gap-3'>
-          <h3>Title: {poll?.poll_title}</h3>
-          <h5>Description: {poll?.poll_description}</h5>
+        <section className='w-5/6 bg-slate-500 p-4 flex flex-col justify-center items-center gap-3 shadow-xl shadow-slate-800 rounded-xl'>
+          <h3 className='text-xl sm:text-2xl'>Title: {poll?.poll_title}</h3>
+          <h5 className='text-lg sm:text-xl'>Description: {poll?.poll_description}</h5>
           <div>{poll?.pollOptions.length! > 0 && poll?.pollOptions.map((pollOption) => (
             <div key={pollOption.poll_option} className='mb-2'>
               <Button 
@@ -139,6 +173,29 @@ const PollDetails = ({
         disabled={isClicked ? false: true}
         >Vote</Button>
     </div>
+
+    <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+          <PieChart>
+          <Pie
+            data={votes}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {votes.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+
     </div>
   )
 }
