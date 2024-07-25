ce19d876a2078bb9a03b010a536787554583a7d2
igachev <ivophoenix1135@gmail.com>
2024-07-25 17:56:51 +0300

    updated Home page
    I added html,css, component CarouselComponent

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,18 @@
+import CarouselComponent from "@/components/CarouselComponent/CarouselComponent";
 
 export default function Home() {
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-full min-h-[800px] bg-stone-700">
+      <h1 className="text-xl sm:text-4xl text-center text-white p-2">Home</h1>
+
+      <section className="w-4/6 mx-auto bg-stone-400 rounded-xl">
+      <h3 className="text-lg sm:text-3xl text-center text-white p-2">Welcome to our voting application!</h3>
+       <CarouselComponent />
+      </section>
+
     </div>
   );
+  
 }
+
